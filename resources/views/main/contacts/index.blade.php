674be6408fb2a79267b040c722bc92d2961610e2
codevsh <105448290+codevsh@users.noreply.github.com>
2023-11-03 22:02:37 +0200

    Change width for icons

--- resources/views/main/contacts/index.blade.php
+++ resources/views/main/contacts/index.blade.php
@@ -41,7 +41,7 @@
                                 </span>random@example.com
                             </li>
                         </ul>
-                        <ul class="list d-flex justify-content-between align-items-center w-50 ps-0">
+                        <ul class="list d-flex justify-content-between align-items-center w-75 ps-0">
                             <li class="list-group-item"><i class="fab fa-linkedin fa-3x  text-secondary "></i></li>
                             <li class="list-group-item"><i class="fab fa-telegram-plane fa-3x  text-secondary "></i></li>
                             <li class="list-group-item"><i class="fab fa-instagram fa-3x  text-secondary "></i></li>
